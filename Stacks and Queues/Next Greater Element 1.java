dc945ed48e0dfb2d8deacfdb228d00f1b7388305
Arhan Kundu <arhankundu99@gmail.com>
2020-04-13 16:30:23 +0530

    Create Next Greater Element 1.java

---
+++ Stacks and Queues/Next Greater Element 1.java
@@ -0,0 +1,29 @@
+//Leetcode problem no 496
+
+//You are given two arrays (without duplicates) nums1 and nums2 where nums1’s elements are subset of nums2. 
+//Find all the next greater numbers for nums1's elements in the corresponding places of nums2.
+//The Next Greater Number of a number x in nums1 is the first greater number to its right in nums2. 
+//If it does not exist, output -1 for this number.
+
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer>stack = new Stack<>();
+        Map<Integer,Integer>map = new HashMap<>();
+        for(int i=0;i<nums2.length;i++)
+        {
+            while(!stack.isEmpty() && stack.peek()<nums2[i])
+                map.put(stack.pop(), nums2[i]);
+            stack.push(nums2[i]);
+        }
+        int[]ret = new int[nums1.length];
+        for(int i=0;i<nums1.length;i++)
+        {
+            if(map.containsKey(nums1[i]))ret[i]=map.get(nums1[i]);
+            else ret[i] = -1;
+        }
+        return ret;
+    }
+}
+
+Idea : Keep decreasing sequence in stack and if we encounter an element x which is greater than stack.peek(),
+then pop until stack.peek() is less than x
