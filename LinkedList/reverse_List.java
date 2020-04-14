80d06098cff32a1a005c3f369d4a2805f4d45d90
Arhan Kundu <arhankundu99@gmail.com>
2020-04-14 14:07:42 +0530

    Update reverse_List.java

--- LinkedList/reverse_List.java
+++ LinkedList/reverse_List.java
@@ -7,6 +7,7 @@
  *     ListNode(int x) { val = x; }
  * }
  */
+// Iterative Solution
 class Solution {
     public ListNode reverseList(ListNode head) {
         ListNode prev = null,next = null,curr = head;
@@ -18,5 +19,23 @@ class Solution {
             curr = next;
         }
         return prev;
+    }
+}
+//Recursive Solution
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head == null)return null;
+        return reverse(null, head);
+    }
+    public ListNode reverse(ListNode prev, ListNode curr)
+    {
+        if(curr.next == null)
+        {
+            curr.next = prev;
+            return curr;
+        }
+        ListNode next = curr.next;
+        curr.next = prev;
+        return reverse(curr, next);
     }
 }
