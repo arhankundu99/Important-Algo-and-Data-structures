d57a467da2113fe16e1f26061a13c396dfaa605c
Arhan Kundu <arhankundu99@gmail.com>
2020-04-21 12:35:53 +0530

    Update matbreak.java

--- Codechef/matbreak.java
+++ Codechef/matbreak.java
@@ -7,7 +7,7 @@ import java.io.*;
 /* Name of the class has to be "Main" only if the class is public. */
 class Codechef
 {
-  static int M = 1000000007;
+  	static int M = 1000000007;
 	public static void main (String[] args) throws java.lang.Exception
 	{
 	    Scanner scan = new Scanner(System.in);
@@ -20,24 +20,21 @@ class Codechef
 	        int prod=1,ans=0;
 	        for(int i=0;i<n;i++)
 	        {
-	            int inc = pow(mult(a, prod), 2 * i + 1);
-			        ans = add(ans,inc);
-			        prod = mult(prod, inc);
+	            int curr = pow(mult(a, prod), 2 * i + 1);
+	  	    ans = add(ans,curr);
+	 	    prod = mult(prod, curr);
 	        }
 	        System.out.println(ans);
 	    }
 	}
 	public static int add(int a,int b){return (int)((0L+a+b)%M);}
-	public static int mult(int a, int b)
-	{
-	    return (int)((a*1L*b)%M);
-	}
+	public static int mult(int a, int b){return (int)((a*1L*b)%M);}
 	public static int pow(int a,int b)
-    {
-        if(b==0)return 1;
-        int p = pow(a,b/2);
-        if(b%2==0)return mult(p,p);
-        return mult(mult(p,p),a);
-    }
+    	{
+        	if(b==0)return 1;
+        	int p = pow(a,b/2);
+        	if(b%2==0)return mult(p,p);
+        	return mult(mult(p,p),a);
+    	}
     
 }
