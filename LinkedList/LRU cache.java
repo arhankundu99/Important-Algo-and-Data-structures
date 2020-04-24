f735d875ffb57ffa5116473cbc83f3fd93708b49
Arhan Kundu <arhankundu99@gmail.com>
2020-04-24 15:35:25 +0530

    Update LRU cache.java

--- LinkedList/LRU cache.java
+++ LinkedList/LRU cache.java
@@ -1,4 +1,4 @@
-*/
+/*
 Design and implement a data structure for Least Recently Used (LRU) cache. It should support the following operations: get and put.
 
 get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
