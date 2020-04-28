47f637ddbd7988961fb62723ef86f629fdf37119
Arhan Kundu <arhankundu99@gmail.com>
2020-04-28 14:32:46 +0530

    Update first unique number.java

--- LinkedList/first unique number.java
+++ LinkedList/first unique number.java
@@ -1,3 +1,4 @@
+/*
 You have a queue of integers, you need to retrieve the first unique integer in the queue.
 
 Implement the FirstUnique class:
@@ -5,6 +6,9 @@ Implement the FirstUnique class:
 FirstUnique(int[] nums) Initializes the object with the numbers in the queue.
 int showFirstUnique() returns the value of the first unique integer of the queue, and returns -1 if there is no such integer.
 void add(int value) insert value to the queue.
+*/
+
+/*linkedHashMap is like hashmap but it preserves the order of insertion*/
 
 class FirstUnique {
     
