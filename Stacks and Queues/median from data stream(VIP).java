235d2dfe71b379bca6252c8d1a383fc99cb20636
Arhan Kundu <arhankundu99@gmail.com>
2020-05-22 16:38:14 +0530

    Create median from data stream(VIP).java

---
+++ Stacks and Queues/median from data stream(VIP).java
@@ -0,0 +1,35 @@
+// leetcode 295 (HARD)
+void addNum(int num) - Add a integer number from the data stream to the data structure.
+double findMedian() - Return the median of all elements so far.
+
+class MedianFinder {
+
+    PriorityQueue<Integer>minHeap, maxHeap;
+    public MedianFinder() {
+        minHeap = new PriorityQueue<>();
+        maxHeap = new PriorityQueue<>(new Comparator<Integer>(){
+            public int compare(Integer a, Integer b){return b-a;}
+        });
+    }
+    
+    public void addNum(int num) {
+        minHeap.add(num);
+        maxHeap.add(minHeap.poll());
+        
+        if(minHeap.size() < maxHeap.size())
+            minHeap.add(maxHeap.poll());
+    }
+    
+    public double findMedian() {
+        if(minHeap.size() == maxHeap.size())
+            return (minHeap.peek()+maxHeap.peek())/2.0;
+        return minHeap.peek();
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
