7fc78bc8f2806d318e51e3ddc0f730946facbc85
Arhan Kundu <arhankundu99@gmail.com>
2020-05-26 15:55:40 +0530

    Update contiguous array.java

--- Two Pointers/contiguous array.java
+++ Two Pointers/contiguous array.java
@@ -5,15 +5,14 @@ class Solution {
     public int findMaxLength(int[] nums) {
         Map<Integer,Integer>map = new HashMap<>();
         map.put(0,-1);
-        int count0=0, count1=0, ret=0;
+        int currSum = 0, ans = 0;
         for(int i=0;i<nums.length;i++)
         {
-            if(nums[i] == 0)count0++;
-            else count1++;
-            int diff = count0-count1;
-            if(map.containsKey(diff))ret = Math.max(ret,i-map.get(diff));
-            else map.put(diff, i);
+            currSum += (nums[i]*2-1); 
+            if(map.containsKey(currSum))
+                ans = Math.max(ans, i-map.get(currSum));
+            else map.put(currSum, i);
         }
-        return ret;
+        return ans;
     }
 }
