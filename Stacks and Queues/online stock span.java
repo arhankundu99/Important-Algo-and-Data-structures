a4ff09460ef3ce07adfe5e4567d123fea9e9ea58
Arhan Kundu <arhankundu99@gmail.com>
2020-06-27 23:13:41 +0530

    Create online stock span.java

---
+++ Stacks and Queues/online stock span.java
@@ -0,0 +1,16 @@
+// 901. Online Stock Span
+class StockSpanner {
+    
+    Stack<int[]>stack;
+    public StockSpanner() {
+        stack = new Stack<>();
+    }
+    
+    public int next(int price) {
+        int count = 1;
+        while(!stack.isEmpty() && stack.peek()[0] <= price)
+            count += stack.pop()[1];
+        stack.push(new int[]{price, count});
+        return count;
+    }
+}
