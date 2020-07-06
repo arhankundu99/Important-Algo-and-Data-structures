56d591bebec113538ae7d15b156ebc2c972a897c
Arhan Kundu <arhankundu99@gmail.com>
2020-07-06 21:45:04 +0530

    Update and rename Container with most water.java to Container with most water(VIP).java

--- Two Pointers/Container with most water(VIP).java
+++ Two Pointers/Container with most water(VIP).java
@@ -1,4 +1,8 @@
 //https://leetcode.com/problems/container-with-most-water/
+//Two pointer technique is used
+//lets say l = 0 and r = n-1
+//so the are would be width * Min(height[l], height[r])
+//now if l < r, then that means that this is already the largest container the left one can possibly form. so we increase l
 class Solution {
     public int maxArea(int[] height) {
          int res=0;
