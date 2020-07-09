792c150f4a7264e0f45c9d45715ebdbf6d19fd66
Arhan Kundu <arhankundu99@gmail.com>
2020-07-09 15:49:21 +0530

    Create pancakeSort(VIP).java

---
+++ Sorting/pancakeSort(VIP).java
@@ -0,0 +1,36 @@
+//https://leetcode.com/problems/pancake-sorting/
+class Solution {
+    public List<Integer> pancakeSort(int[] A) {
+        int lastIdx = A.length-1;
+        List<Integer>list = new ArrayList<>();
+        for(int i = 0; i < A.length; i++){
+            int idx = findLargestNumberIdx(A, lastIdx);
+            flip(A, idx+1);
+            flip(A, lastIdx+1);
+            list.add(idx+1);
+            list.add(lastIdx+1);
+            lastIdx--;
+        }
+        return list;
+    }
+    public int findLargestNumberIdx(int[]A, int idx){
+        for(int i = 0; i < A.length; i++){
+            if(A[i] == idx+1)return i;
+        }
+        return -1;
+    }
+    
+    public void flip(int[]A, int k){
+        int idx1 = 0;
+        int idx2 = k-1;
+        
+        while(idx1 < idx2){
+            int temp = A[idx1];
+            A[idx1] = A[idx2];
+            A[idx2] = temp;
+            
+            idx1++;
+            idx2--;
+        }
+    }
+}
