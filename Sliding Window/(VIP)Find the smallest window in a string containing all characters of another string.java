d6b26c80d91e801103dc4cab5a473a90f97ef9f5
Arhan Kundu <arhankundu99@gmail.com>
2020-07-16 16:19:31 +0530

    Update (VIP)Find the smallest window in a string containing all characters of another string.java

--- Sliding Window/(VIP)Find the smallest window in a string containing all characters of another string.java
+++ Sliding Window/(VIP)Find the smallest window in a string containing all characters of another string.java
@@ -3,6 +3,8 @@
 //Given two strings string1 and string2, the task is to find the smallest substring in string1 containing all characters of string2 efficiently.
 // very important
 
+/*package whatever //do not write package name here */
+
 import java.util.*;
 import java.lang.*;
 import java.io.*;
@@ -24,32 +26,24 @@ class GFG {
 	    int minLength = Integer.MAX_VALUE;
 	    
 	    Map<Character, Integer>map = new HashMap<>();
-	    Map<Character, Integer>currMap = new HashMap<>();
 	    int count = t.length();
 	    
 	    for(int i = 0; i < t.length(); i++){
-	        if(!map.containsKey(t.charAt(i)))
-	        {
-	            map.put(t.charAt(i), 1);
-	            currMap.put(t.charAt(i), 1);
-	        }
-	        else{
-	            map.put(t.charAt(i), map.get(t.charAt(i))+1);
-	            currMap.put(t.charAt(i), currMap.get(t.charAt(i))+1);
-	        }
+	        if(!map.containsKey(t.charAt(i)))map.put(t.charAt(i), 1);
+	        else map.put(t.charAt(i), map.get(t.charAt(i))+1);
 	    }
 	    int j = 0;
 	    int idx1 = -1, idx2 = -1;
 	    for(int i = 0; i < s.length(); i++){
 	        if(map.containsKey(s.charAt(i))){
-	            if(currMap.get(s.charAt(i)) > 0)count--;
-	            currMap.put(s.charAt(i), currMap.get(s.charAt(i))-1);
+	            if(map.get(s.charAt(i)) > 0)count--;
+	            map.put(s.charAt(i), map.get(s.charAt(i))-1);
 	            if(count == 0){
 	                //now we increase j until we reach smaller substring
 	                while(true){
-	                    if(currMap.containsKey(s.charAt(j))){
-	                        if(currMap.get(s.charAt(j)) == 0)break;
-	                        currMap.put(s.charAt(j), currMap.get(s.charAt(j))+1);
+	                    if(map.containsKey(s.charAt(j))){
+	                        if(map.get(s.charAt(j)) == 0)break;
+	                        map.put(s.charAt(j), map.get(s.charAt(j))+1);
 	                    }
 	                    j++;
 	                }
