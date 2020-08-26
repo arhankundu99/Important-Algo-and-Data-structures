325d6923310181ab0f31606b6783bc95d96d0124
Arhan Kundu <arhankundu99@gmail.com>
2020-08-26 22:12:49 +0530

    Update max-sum-of-rectangle-no-larger-than-k.java

--- Dynamic Programming/dp and matrix/max-sum-of-rectangle-no-larger-than-k.java
+++ Dynamic Programming/dp and matrix/max-sum-of-rectangle-no-larger-than-k.java
@@ -1,28 +1,29 @@
 // https://leetcode.com/problems/max-sum-of-rectangle-no-larger-than-k/
-
+// O(n^3logn) solution
 class Solution {
-    public int maxSumSubmatrix(int[][] matrix, int target) {
+    public int maxSumSubmatrix(int[][] matrix, int k) {
         for(int i = 0; i < matrix.length; i++){
             for(int j = 1; j < matrix[0].length; j++){
                 matrix[i][j] += matrix[i][j-1];
             }
         }
         int ans = Integer.MIN_VALUE;
-        for(int i = 0; i < matrix[0].length; i++){
-            for(int j = i; j < matrix[0].length; j++){
-                int r = 0;
-                while(r < matrix.length){
-                    int sum = 0;
-                    for(int k = r; k < matrix.length; k++){
-                        if(i > 0)sum += matrix[k][j] - matrix[k][i-1];
-                        else sum += matrix[k][j];
-                        if(sum <= target)ans = Math.max(ans, sum);
-                    }
-                    r++;
+        for(int c1 = 0; c1 < matrix[0].length; c1++){
+            for(int c2 = c1; c2 < matrix[0].length; c2++){
+                
+                TreeSet<Integer>set = new TreeSet<>();
+                int sum = 0;
+                set.add(0);
+                
+                for(int r = 0; r < matrix.length; r++){
+                    sum += (c1 == 0)? matrix[r][c2]: matrix[r][c2] - matrix[r][c1-1];
+                    Integer find = set.ceiling(sum - k);
+                        
+                    if(find != null)ans = Math.max(ans, sum - find);
+                    set.add(sum);
                 }
             }
         }
         return ans;
-        
     }
 }
