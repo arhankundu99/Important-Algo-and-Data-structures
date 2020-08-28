7b2630f0da4f7d07588a3bf64f21e26fce107dee
Arhan Kundu <arhankundu99@gmail.com>
2020-08-28 23:28:51 +0530

    Update Best time to buy and sell stock 2.java

--- Dynamic Programming/Stock Buy and Sell/Best time to buy and sell stock 2.java
+++ Dynamic Programming/Stock Buy and Sell/Best time to buy and sell stock 2.java
@@ -1,6 +1,26 @@
 // https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
 // here we can engage in multiple transactions and buy and sell on the same day.
 
+// do this in interviews
+class Solution {
+    public int maxProfit(int[] prices) {
+        int i = 0, peak = -1, valley = -1, maxProfit = 0;
+        
+        while(i < prices.length-1){
+            while(i < prices.length-1 && prices[i] >= prices[i+1])i++;
+            
+            valley = prices[i];
+            
+            while(i < prices.length-1 && prices[i] <= prices[i+1])i++;
+            
+            peak = prices[i];
+            
+            maxProfit += peak - valley;
+        }
+        return maxProfit;
+    }
+}
+
 class Solution {
     public int maxProfit(int[] prices) {
         int ans = 0;
