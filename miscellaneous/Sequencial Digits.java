38ebc4343374d9213cfec059858c27246705b178
Arhan Kundu <arhankundu99@gmail.com>
2020-09-19 18:07:27 +0530

    Update Sequencial Digits.java

--- miscellaneous/Sequencial Digits.java
+++ miscellaneous/Sequencial Digits.java
@@ -19,3 +19,19 @@ class Solution {
         return res;
     }
 }
+
+class Solution {
+    public List<Integer> sequentialDigits(int low, int high) {
+        List<Integer> ans = new ArrayList<>();
+        Queue<Integer> q = new LinkedList<>();
+        if(low <= 0 && high >= 0) ans.add(0);
+        for(int i = 1; i < 10; i++) q.add(i);
+        while(q.size() > 0){
+            int curr = q.remove();
+            if(curr >= low && curr <= high) ans.add(curr);
+            int onesDigit = curr % 10;
+            if(onesDigit < 9 && curr * 10 + onesDigit + 1 <= high) q.add(curr * 10 + onesDigit + 1);
+        }
+        return ans;
+    }
+}
