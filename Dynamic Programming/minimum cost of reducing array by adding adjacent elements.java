d92a8f9c1a61ac84bdf6c5cf11b72b82c4e1ecbf
Arhan Kundu <arhankundu99@gmail.com>
2020-09-24 21:40:28 +0530

    Update minimum cost of reducing array by adding adjacent elements.java

--- Dynamic Programming/minimum cost of reducing array by adding adjacent elements.java
+++ Dynamic Programming/minimum cost of reducing array by adding adjacent elements.java
@@ -39,3 +39,47 @@ class GFG {
 	    return ans;
 	}
 }
+
+//GFG Solution: Bottom up approch
+static int mergeTwoNumbers(int []numbers) 
+{ 
+    int len, i, j, k; 
+    int n = numbers.length; 
+    if (numbers.length == 0) 
+    { 
+        return 0; 
+    } 
+  
+    int []prefixSum = new int[n + 1]; 
+
+    for (i = 1; i <= n; i++)  
+    { 
+        prefixSum[i] = prefixSum[i - 1] + 
+                         numbers[i - 1]; 
+    } 
+   
+    int [][]dp = new int[n + 1][n + 1]; 
+  
+    for (len = 2; len <= n; len++)  
+    { 
+  
+        for (i = 1; i <= n - len + 1; i++) 
+        { 
+            j = i + len - 1; 
+
+            int sum = prefixSum[j] -  
+                      prefixSum[i - 1]; 
+  
+            dp[i][j] = Integer.MAX_VALUE; 
+
+            for (k = i; k < j; k++)  
+            { 
+                dp[i][j] = Math.min(dp[i][j],  
+                                    dp[i][k] + 
+                                    dp[k + 1][j] +  
+                                    sum); 
+            } 
+        } 
+    } 
+    return dp[1][n]; 
+} 
