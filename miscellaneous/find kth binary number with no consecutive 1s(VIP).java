f552d49a49f04f1bb0ab92ed92047f564efc09f3
Arhan Kundu <arhankundu99@gmail.com>
2020-09-29 17:56:38 +0530

    Update find kth binary number with no consecutive 1s(VIP).java

--- miscellaneous/find kth binary number with no consecutive 1s(VIP).java
+++ miscellaneous/find kth binary number with no consecutive 1s(VIP).java
@@ -2,50 +2,41 @@
 
 /*package whatever //do not write package name here */
 
-import java.io.*;
-import java.util.*;
+
+
+
 import java.util.*;
 
-class GFG {
-	public static void main (String[] args) {
+
+// Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+
+class TestClass {
+    public static void main(String args[] ) throws Exception {
+		long[] fib = new long[50];
+		fib[0] = 1;
+		fib[1] = 2;
+
+		for(int i = 2; i < 40; i++)fib[i] = fib[i-1] + fib[i-2];
+
 		Scanner scan = new Scanner(System.in);
-		Map<Integer, Integer>map = new HashMap<>();
-		map.put(1, 1);
-		map.put(2, 2);
-		map.put(3, 3);
-		
-		int key = 4;
-		int maxKey = 3;
-		while(true){
-		    map.put(key, map.get(key-1)+map.get(key-2));
-		    if(map.get(key) >= 100000000)break;
-		    key++;
-		    maxKey = Math.max(key, maxKey);
-		}
 		int t = scan.nextInt();
-		while(t-->0){
-		    int k = scan.nextInt();
-		    
-		    long ans = 0;
-		
-		    
-		    while(k!= 0){
-		        //System.out.println(idx);
 
-		        for(int i = 1; i <= maxKey; i++){
-		            if(map.get(i) == k){
-		                ans += (long)Math.pow(2, i-1);
-		                System.out.println(Long.toBinaryString(ans));
-		                k=0;
-		                break;
-		            }
-		            if(map.get(i) > k){
-		                ans += (long)Math.pow(2, i-2);
-		                k = k - (map.get(i-1));
-		                break;
-		            }
-		        }
-		    }
-	    }
+		while(t-->0){
+			int n = scan.nextInt();
+			System.out.println(Long.toBinaryString(solve(n, fib)));
+		}
+    }
+	public static long solve(int n, long[] fib){
+		long ans = 0;
+		int f = 0;
+
+		for(int i = 39; i >= 0; i--){
+			if(fib[i] <= n){
+				ans += (1L << i);
+				n -= fib[i];
+			}
+		}
+		return ans;
 	}
 }
+
