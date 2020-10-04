5d287e8108fd55b8c63c72f384f0a892bb33f887
Arhan Kundu <arhankundu99@gmail.com>
2020-10-04 15:21:42 +0530

    Update Remove Covered Intervals.java

--- Greedy/Remove Covered Intervals.java
+++ Greedy/Remove Covered Intervals.java
@@ -6,15 +6,11 @@ class Solution {
                 return a[0] == b[0]? b[1] - a[1]: a[0] - b[0];
             }
         });
-        
-        int start = intervals[0][0];
         int end = intervals[0][1];
         int count = 1;
         
         for(int i = 1; i < intervals.length; i++){
-            if(intervals[i][1] <= end && intervals[i][0] >= start)continue;
-            
-            start = intervals[i][0];
+            if(intervals[i][1] <= end)continue;
             end = intervals[i][1];
             count++;
         }
