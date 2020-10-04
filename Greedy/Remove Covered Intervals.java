69ebf0073c12a0d0676d276a135a38f36e49d013
Arhan Kundu <arhankundu99@gmail.com>
2020-10-04 15:19:45 +0530

    Create Remove Covered Intervals.java

---
+++ Greedy/Remove Covered Intervals.java
@@ -0,0 +1,23 @@
+// https://leetcode.com/explore/challenge/card/october-leetcoding-challenge/559/week-1-october-1st-october-7th/3483/
+class Solution {
+    public int removeCoveredIntervals(int[][] intervals) {
+        Arrays.sort(intervals, new Comparator<int[]>(){
+            public int compare(int[] a, int[] b){
+                return a[0] == b[0]? b[1] - a[1]: a[0] - b[0];
+            }
+        });
+        
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+        int count = 1;
+        
+        for(int i = 1; i < intervals.length; i++){
+            if(intervals[i][1] <= end && intervals[i][0] >= start)continue;
+            
+            start = intervals[i][0];
+            end = intervals[i][1];
+            count++;
+        }
+        return count;
+    }
+}
