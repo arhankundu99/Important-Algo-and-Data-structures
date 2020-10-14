79564d3f83af7c3e6c9ae7e65956acade11b133d
Arhan Kundu <arhankundu99@gmail.com>
2020-10-14 15:19:02 +0530

    Update Super pow.java

--- Math/Super pow.java
+++ Math/Super pow.java
@@ -3,12 +3,12 @@ class Solution {
     int mod = 1337;
     public int superPow(int a, int[] b) {
         int ans = 1;
-		    a %= mod;
+	a %= mod;
         
-		    //suppose a = 3 and b = [1, 2, 3, 4]
-		    //when i = 0, ans = 3 ^ 1
-		    //when i = 1, ans = ((3 ^ 1) ^ 10) * (3 ^ 2)) = 3 ^ (12)
-		    //when i = 2, ans = ((3 ^ 12) ^ 10) * (3 ^ 3)) = 3 ^ (123) and so on....
+	//suppose a = 3 and b = [1, 2, 3, 4]
+	//when i = 0, ans = 3 ^ 1
+	//when i = 1, ans = ((3 ^ 1) ^ 10) * (3 ^ 2)) = 3 ^ (12)
+	//when i = 2, ans = ((3 ^ 12) ^ 10) * (3 ^ 3)) = 3 ^ (123) and so on....
         for(int i = 0; i < b.length; i++){
             ans = (pow(ans, 10) * pow(a, b[i])) % mod;
         }
