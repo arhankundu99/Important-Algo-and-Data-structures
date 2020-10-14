2375b9c2cdc0bdd3f442758a9cf4b73d4c34e5c0
Arhan Kundu <arhankundu99@gmail.com>
2020-10-14 15:33:23 +0530

    Create LIS (nlogn).java

---
+++ Dynamic Programming/LIS (nlogn).java
@@ -0,0 +1,57 @@
+/*package whatever //do not write package name here */
+// https://practice.geeksforgeeks.org/problems/longest-increasing-subsequence/0
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int t = scan.nextInt();
+		while(t-->0){
+		    int n = scan.nextInt();
+		    int[] a = new int[n];
+		    for(int i = 0; i < n; i++)a[i] = scan.nextInt();
+		    System.out.println(LIS(a, n));
+		}
+	}
+	static int CeilIndex(int A[], int l, int r, int key) 
+    { 
+        while (r - l > 1) { 
+            int m = l + (r - l) / 2; 
+            if (A[m] >= key) 
+                r = m; 
+            else
+                l = m; 
+        } 
+  
+        return r; 
+    } 
+  
+    static int LIS(int A[], int size) 
+    { 
+        // Add boundary case, when array size is one 
+  
+        int[] tailTable = new int[size]; 
+        int len; // always points empty slot 
+  
+        tailTable[0] = A[0]; 
+        len = 1; 
+        for (int i = 1; i < size; i++) { 
+            if (A[i] < tailTable[0]) 
+                // new smallest value 
+                tailTable[0] = A[i]; 
+  
+            else if (A[i] > tailTable[len - 1]) 
+                // A[i] wants to extend largest subsequence 
+                tailTable[len++] = A[i]; 
+  
+            else
+                // A[i] wants to be current end candidate of an existing 
+                // subsequence. It will replace ceil value in tailTable 
+                tailTable[CeilIndex(tailTable, -1, len - 1, A[i])] = A[i]; 
+        } 
+  
+        return len; 
+    } 
+}
