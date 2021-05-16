b3bc646202f68feb8f94c87d8b4abe3207cfb01f
Arhan Kundu <arhankundu99@gmail.com>
2021-05-16 17:03:16 +0530

    Update Binary Tree Cameras (VVIP).java

--- Trees/Binary Tree Cameras (VVIP).java
+++ Trees/Binary Tree Cameras (VVIP).java
@@ -1,4 +1,19 @@
 // https://leetcode.com/explore/challenge/card/may-leetcoding-challenge-2021/600/week-3-may-15th-may-21st/3745/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     int count = 0;
     public int minCameraCover(TreeNode root) {
@@ -14,15 +29,16 @@ class Solution {
         int left = dfs(root.left);
         int right = dfs(root.right);
         
-        
         if(left == 0 && right == 0){
             //this means both root.left and root.right are covered. therefore root needs camera camera coverage
             return -1;
         }
         if(left == -1 || right == -1){
+            // this means both root.left and root.right need camera coverage. So a camera should be placed at root.
             count++;
             return 1;
         }
+        //root.left or root.right has a camera. so root does not need camera coverage
         return 0;
         
     }
