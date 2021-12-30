808adabd014d6d8682eaeda19beeb6f8d45e0bec
Arhan Kundu <arhankundu99@gmail.com>
2021-12-30 14:20:55 +0530

    Update Populating Next Right Pointers in Each Node(VIP).java

--- Trees/Populating Next Right Pointers in Each Node(VIP).java
+++ Trees/Populating Next Right Pointers in Each Node(VIP).java
@@ -19,7 +19,56 @@ class Node {
     }
 };
 */
+class Solution {
+    public Node connect(Node root) {
+        return connectIterative(root);                
 
+    }
+    //O(1) space and O(N) time where N is the number of nodes
+    public Node connectIterative(Node root){
+        if(root == null)return null;
+                
+        Node curr = root;
+        
+        while(curr.left != null){
+            
+            Node parent = curr;
+            Node child = curr.left;
+            
+            while(parent != null){
+                child.next = parent.right;
+                child = child.next;
+                
+                while(parent != null){
+                    parent = parent.next;
+                    if(parent == null)break;
+                    
+                    child.next = parent.left;
+                    child = child.next;
+                    
+                    child.next = parent.right;
+                    child = child.next;
+                }
+            }
+            
+            curr = curr.left;
+        }
+        return root;
+    }
+    //O(h) space and O(n) time, where h is the height of the tree
+    public void connectDFS(Node root){
+        if(root == null)return;
+        Node curr = root;
+        
+        while(curr != null && curr.left != null){
+            curr.left.next = curr.right; 
+            if(curr.next != null)curr.right.next = curr.next.left;
+            curr = curr.next;
+        }
+        connectDFS(root.left);
+        connectDFS(root.right);
+    }
+}
 class Solution {
     Map<Integer, Node> map;
     public Node connect(Node root) {
